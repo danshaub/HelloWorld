13b72bcfaac1e2d771202515b04cec495718d381
danshaub <haub@chapman.edu>
2019-02-05 22:47:25 +0000

    Added comments

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,3 +1,11 @@
+/*
+Name:  Dan Haub
+Student ID#:  2315346
+Chapman Email:  haub@chapman.edu
+Course Number and Section:  CPSC 350-01
+Assignment:  1 - Hello World
+*/
+
 #include <iostream>
 
 int main(){
