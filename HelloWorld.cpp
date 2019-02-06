521e73746dc5ce5a21ac1bee6865996de2bdab73
Dan Haub <43016331+danshaub@users.noreply.github.com>
2019-02-05 21:48:17 -0800

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,7 @@ Name:  Dan Haub
 Student ID#:  2315346
 Chapman Email:  haub@chapman.edu
 Course Number and Section:  CPSC 350-01
-Assignment:  1 - Hello World
+Assignment:  Hello World
 */
 
 #include <iostream>
